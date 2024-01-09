7ad7a47348cbcfc5c917dcd08c4a46944d010774
unknown <gabebarraza33@gmail.com>
2024-01-09 00:26:16 -0600

    Testing the code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // Create an array of questions for user input
 function userInput(){
